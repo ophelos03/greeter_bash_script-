0fde18034bbb9e37b51b3834538fb5d7a4e9ea1b
ophelos03 <badmus676@gmail.com>
2025-10-16 20:35:13 +0100

    This is for adding the greeter.sh script to the repo

---
+++ greeter.sh
@@ -0,0 +1,13 @@
+#This is a script that asks the user for their name and then prints a personalized greeting
+#
+#
+#
+#
+#
+#
+#
+#
+
+#!/usr/bin/bash        #shebang_line
+read -p "Please enter your name: " name
+echo "hello, $name Nice to meet You"
